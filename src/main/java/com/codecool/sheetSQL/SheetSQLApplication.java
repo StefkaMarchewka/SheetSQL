cbe9b237b4b8a6f18caf848d20210738d8c2ea14
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-18 13:05:04 +0100

    basic spring boot app

---
+++ src/main/java/com/codecool/sheetSQL/SheetSQLApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.sheetSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetSQLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetSQLApplication.class, args);
+    }
+
+}
