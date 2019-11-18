11c917b37d657d2b8a9e6f72d50f2a54ffafe202
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-18 13:39:02 +0100

    my main from si

--- src/main/java/com/codecool/sheetSQL/controllers/MainController.java
+++ src/main/java/com/codecool/sheetSQL/controllers/MainController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 
 @Controller
 public class MainController {
@@ -28,6 +32,39 @@ public class MainController {
         //bla
         return "there should be query result";
     }
+
+//    public String stefaniasSIMainMenuVersion(){
+//        try {
+//            List<String> data = csvReader.getDataFromCSV("src/main/resources/csv_test.csv");
+//            List<String> argsList = Arrays.asList(args);
+//            if(argsList.size() == 4 && argsList.contains("*")){
+//                printResults(csvQueries.getAllData(data));
+//            } else if(argsList.size() == 4){
+//                //show content of one column
+//                String columnName = argsList.get(1);
+//                List<String> contentOfOneColumn = csvQueries.getContentOfChosenColumn(data, columnName);
+//                printResults(contentOfOneColumn);
+//            }else if(argsList.size() == 8 && String.join("", args).contains("count")){
+//                String columnName = getColumnNameFromArgs(args);
+//                String valueToFind = argsList.get(7);
+//                System.out.println(csvQueries.countResults(data, columnName, valueToFind));
+//            }else if(argsList.size() == 8) {
+//                //chose row by selected columns value
+//                String column = argsList.get(5);
+//                String valueToFind = argsList.get(7);
+//                printResults(csvQueries.getRowByChosenColumnValue(data, column, valueToFind));
+//            }
+////                else {
+////                    List<String> columns = new ArrayList<>();
+////                    columns.add("city");
+////                    columns.add("age");
+////                    printResults(csvQueries.getChosenColumnsFromRow(data, columns));
+////                }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
 }
