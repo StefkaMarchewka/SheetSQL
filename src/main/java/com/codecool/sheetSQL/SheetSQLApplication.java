0f5c7040a8f116d9cad15bb9e47edc9146afdf89
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-19 14:21:41 +0100

    app works

--- src/main/java/com/codecool/sheetSQL/SheetSQLApplication.java
+++ src/main/java/com/codecool/sheetSQL/SheetSQLApplication.java
@@ -1,10 +1,12 @@
 package com.codecool.sheetSQL;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SheetSQLApplication {
+public class SheetSQLApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SheetSQLApplication.class, args);
