f10cba652bd70ecf8eade9c2771002c2dabe3253
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-19 12:13:13 +0100

    refactor

--- src/main/java/com/codecool/sheetSQL/model/UserData.java
+++ src/main/java/com/codecool/sheetSQL/model/UserData.java
@@ -7,17 +7,8 @@ public class UserData {
     private String spreadsheetId;
     private String range;
 
-    public void setSpreadsheetId(String spreadsheetId) {
-        this.spreadsheetId = spreadsheetId;
-    }
-
-    public void setRange(String range) {
-        this.range = range;
-    }
-
     public UserData(){
     }
-
 
     public String getSpreadsheetId() {
         return spreadsheetId;
@@ -25,5 +16,13 @@ public class UserData {
 
     public String getRange() {
         return range;
+    }
+
+    public void setSpreadsheetId(String spreadsheetId) {
+        this.spreadsheetId = spreadsheetId;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
     }
 }
