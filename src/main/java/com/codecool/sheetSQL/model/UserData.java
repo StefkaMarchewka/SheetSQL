1f9eefa56499c35b4ba84d53d35acaeb3e58e810
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-19 12:12:38 +0100

    add: set UserData fields with setters

--- src/main/java/com/codecool/sheetSQL/model/UserData.java
+++ src/main/java/com/codecool/sheetSQL/model/UserData.java
@@ -1,15 +1,19 @@
 package com.codecool.sheetSQL.model;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserData {
-
-    @Value("string value")
     private String spreadsheetId;
-    @Value("string value")
     private String range;
+
+    public void setSpreadsheetId(String spreadsheetId) {
+        this.spreadsheetId = spreadsheetId;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
 
     public UserData(){
     }
