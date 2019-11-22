b26c612ac8c3329ca6995cb9097718fc78b97561
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-22 12:00:52 +0100

    changed port num to fix number (to presentation purpose)

--- src/main/java/com/codecool/sheetSQL/repository/GoogleSpreadSheetRepository.java
+++ src/main/java/com/codecool/sheetSQL/repository/GoogleSpreadSheetRepository.java
@@ -81,7 +81,7 @@ public class GoogleSpreadSheetRepository implements DataReaderInterface{
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(portNum).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
 
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
